7559128d723fd1c31b53eac60634a7c381fcde4e
Han Bing Feng <hangoglebf@gmail.com>
2015-05-26 15:57:21 +0800

    fix keywordii bin

--- bin/run_pipeline_basic_keywordii.sh
+++ bin/run_pipeline_basic_keywordii.sh
@@ -4,7 +4,7 @@ source `dirname $0`/run_env.sh
 
 export PIPELINE_NAME=pipeline_basic_performance_nirvana.json
 export LOG_NAME=fengchao_feview_performance_rawlog_speedup3_day
-export DB_COLLECTION_NAME=performance_keyword_basic
+export DB_COLLECTION_NAME=performance_keywordii_basic
 export TARGET_PATH=/manage/keyword
 
 call_get_item
