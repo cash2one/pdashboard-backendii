0985703bdcaa9b160bfdfeeef80610fb1ec8dc76
gushouchuang <gushouchuang@baidu.com>
2015-06-01 15:54:23 +0800

    sh debug

--- bin/run_pipeline_keyword_corefunction.sh
+++ bin/run_pipeline_keyword_corefunction.sh
@@ -4,6 +4,10 @@ source `dirname $0`/run_env.sh
 
 export PIPELINE_NAME=pipeline_performance_keyword_file.json
 export LOG_NAME=fengchao_feview_performance_nirvana_mod_day
-export DB_COLLECTION_NAME="[\"performance_keyword_coreFunction\",\"performance_keywordii_coreFunction\"]"
+export DB_COLLECTION_NAME={\"new\":\"performance_keyword_coreFunction\",\"old\":\"performance_keywordii_coreFunction\"}
+export TARGET_PATH=/manage/keyword
+
+export DB_URL=performance_GET_data
+
 call_get_item
 
