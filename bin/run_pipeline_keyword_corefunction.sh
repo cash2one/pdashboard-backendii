e580f4e5a89e505b2f029f24c145da2ef97e2911
Han Bing Feng <hangoglebf@gmail.com>
2015-06-03 14:13:26 +0800

    update param

--- bin/run_pipeline_keyword_corefunction.sh
+++ bin/run_pipeline_keyword_corefunction.sh
@@ -5,7 +5,7 @@ source `dirname $0`/run_env.sh
 export PIPELINE_NAME=pipeline_performance_keyword_file.json
 export LOG_NAME=fengchao_feview_performance_nirvana_mod_day
 export DB_COLLECTION_NAME=\{\\\"new\\\":\\\"performance_keywordii_coreFunction\\\",\\\"old\\\":\\\"performance_keyword_coreFunction\\\"\}
-export START=20150526
+
 export TARGET_PATH=/manage/keyword
 
 call_get_item
