da3ac410bc3a1cfa02113e157eaed0c514e0fac4
Han Bing Feng <hangoglebf@gmail.com>
2015-06-25 12:03:40 +0800

    add xll 3

--- bin/run_pipeline_basic_keywordii.sh
+++ bin/run_pipeline_basic_keywordii.sh
@@ -6,7 +6,7 @@ export PIPELINE_NAME=pipeline_basic_performance_nirvana.json
 export LOG_NAME=fengchao_feview_performance_rawlog_speedup3_day
 export DB_COLLECTION_NAME=performance_keywordii_basic
 export TARGET_PATH=/manage/keyword
-export XLL="[\"xll/speedup3.keyword.1\",\"xll/speedup3.keyword.2\"]"
+export XLL="[\"xll/speedup3.keyword.1\",\"xll/speedup3.keyword.2\",\"xll/speedup3.keyword.3\"]"
 
 call_get_item
 
