5b3d6649b6b112caa6fdeeb8d350b8d29d877db9
wujianwei01 <wujianwei01@baidu.com>
2015-09-16 20:02:30 +0800

    add a js deploy file  and a monitor shell
    - add a deploy js and save the conf as json file
    - add a monitor shell

---
+++ backend_daemon.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+##
+# @file 后台服务守护进程
+# @author wujianwei01@baidu.com
+#
+##
+
+# 检查当前目录中是否存在edp-webserver-config.js文件
+test -f ./edp-webserver-config.js || exit 0
+
+# 执行的命令
+CMD="nohup edp webserver start --port 18848 --dbUrl  mongodb://localhost:27017/pdashboard &"
+
+# pid存放文件
+PID=.pdashboard-backend.pid
+
+# 运行日志存放文件
+LOG=~/.pdashboard-backend-daemon.log
+
+# 启动
+function start {
+    $CMD  >> $LOG 2>&1 &
+    myPid=$!
+    echo $myPid > $PID
+    echo "service start"
+}
+
+# 停止
+function stop {
+    kill `cat $PID`
+    rm $PID
+    echo "service stop"
+}
+
+echo "$CMD $1"
+
+case "$1" in
+start)
+    start
+    ;;
+stop)
+    stop
+    exit 0
+    ;;
+restart)
+    if [ -f $PID ]; then
+        stop
+        sleep 3
+    fi
+    start
+    ;;
+esac
---
+++ deploy.js
@@ -0,0 +1,93 @@
+/**
+ * @file 项目部署文件
+ * @author wujianwei01@baidu.com
+ * 
+ */
+
+var prompt = require('prompt');
+var fs = require('fs-extra');
+var _ = require('underscore');
+
+// 文件夹不存在时的提示语
+var dirNotExistMsg = '文件夹不存在，请重新输入';
+
+/**
+ * 判断文件夹是否存在
+ * @param {string} dirname 文件夹名
+ * @return {boolean} true|false 存在返回true,否则false
+ *
+ */
+function isDirExist (dirname) {
+    return !fs.existsSync(dirname) ? false : true;
+}
+var schema = {
+    properties: {
+        'hadoop-bin': {
+            require: true,
+            description: '请输入hadoop-bin目录绝对路径',
+            message: dirNotExistMsg,
+            conform: isDirExist
+        },
+        'log-cluster': {
+            require: true,
+            description: '请输入日志文件所在的集群路径'
+        },
+        'lsp-path': {
+            require: true,
+            description: '请输入集群中lsp路径'
+        },
+        'work-home': {
+            require: true,
+            description: '请输入项目工作目录所在的绝对路径',
+            message: dirNotExistMsg,
+            conform: isDirExist
+        },
+        'db-url': {
+            require: true,
+            description: '请输入后端mongodb地址'
+        },
+        'data-path': {
+            require: true,
+            description: '请输入下载完成后的文件存放目录'
+        },
+        'log-path': {
+            require: true,
+            description: '请输入项目工作日志存放目录'
+        }
+    }
+};
+
+var watchingList = {
+    'watching-list': [
+        {
+            "name": "fengchao_feview_performance_jsonlog_nirvanaII_json_daily",
+            "period": "daily",
+            "collection": []
+        },
+        {
+            "name": "fengchao_feview_performance_jsonlog_nirvana_json_daily",
+            "period": "daily",
+            "collection": []
+        },
+        {
+            "name": "fengchao_feview_performance_jsonlog_nirvanaII_json_hourly",
+            "period": "hourly",
+            "collection": []
+        },
+        {
+            "name": "fengchao_feview_performance_jsonlog_nirvana_json_hourly",
+            "period": "hourly",
+            "collection": []
+        }
+    ]
+};
+prompt.start();
+
+prompt.get(schema, function (err, result) {
+    if (err) {
+        console.log(err);
+    }
+    else {
+        fs.writeFileSync('~/.nightingale.conf', JSON.stringify(_.extend(result, watchingList), null, 4), 'utf-8');
+    }
+});
