6eb5c53f462d4e9c3a0d38b30072d76ff3f2c830
Pride Leong <lykling.lyk@gmail.com>
2015-11-07 16:00:28 +0800

    Fix script variables

--- crontab_hourly.sh
+++ crontab_hourly.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 source $HOME/.bashrc
-WORKKING_DIR=${HOME}/.local/var/pdashboard-backend
-node ${HOME}/.local/var/pdashboard-backend/bin/run_hourly.js >> ${HOME}/logs/pdashboard_out.log
+WORKING_DIR=${HOME}/.local/var/pdashboard-backend
+node ${HOME}/.local/var/pdashboard-backend/bin/run_hourly.js >> ${WORKING_DIR}/logs/pdashboard_out.log
 lasthour=`date -d"-1 hours" +"%Y-%m-%d %H:00:00"`
 datapath="/var/nirvana/raw_live_logs/data_slice"
-ruby ${WORKKING_DIR}/bin/adpreview_backend_request_stat.rb -t "${lasthour}" -p ${datapath} >> ${WORKKING_DIR}/logs/pdashboard_out.log
+ruby ${WORKKING_DIR}/bin/adpreview_backend_request_stat.rb -t "${lasthour}" -p ${datapath} >> ${WORKING_DIR}/logs/pdashboard_out.log
 echo "${lasthour}"
