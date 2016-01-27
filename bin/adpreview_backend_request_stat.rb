7358418ec9f09f8894ec04bec5c9fa09739b23f4
gushouchuang <gushouchuang@baidu.com>
2016-01-27 15:32:22 +0800

    oo ruby function

--- bin/adpreview_backend_request_stat.rb
+++ bin/adpreview_backend_request_stat.rb
@@ -16,6 +16,45 @@ require "optparse"
 require "optparse/date"
 require "optparse/time"
 
+# config配置
+config = [{
+  "title": "人工请求/机器请求/后端请求总数",
+  "collName": ":adpreview_backend_request_stat",
+  "collNameHoruly": ":adpreview_backend_request_stat_hourly"
+},
+{
+  "title": "召回率/准确率",
+  "collName": ":adpreview_backend_recall_precision",
+  "collNameHoruly": ":adpreview_backend_recall_precision_hourly"
+},
+{
+  "title": "单个策略贡献/累计策略贡献/策略名映射",
+  "collName": ":adpreview_backend_strategy_contribution",
+  "collNameHoruly": ":adpreview_backend_strategy_contribution_hourly"
+}]
+
+# Function 统一db入库操作 define
+# options ruby命令行参数集合
+# config 任务参数详情
+# recordTimestamp 时间戳
+# client db
+def db_insert_data (options, config, recordTimestamp, client)
+  for value in config
+    value["data"]["recordTimestamp"] = recordTimestamp
+    puts value["title"] # head
+    puts value["data"] # 数据明细
+
+    collName = value["collName"]
+    collName = value["collNameHoruly"] if !options[:time].nil?
+    coll = client[collName, :capped => true]
+    coll.delete_many(:recordTimestamp => recordTimestamp)
+    coll.insert_one(value["data"])
+    result = coll.find()
+    puts result # collection全数据
+  end
+end
+
+
 options = {}
 puts ARGV
 OptionParser.new do |opts|
@@ -43,33 +82,23 @@ recordTimestamp = recordTimestamp - (8 * 3600 * 1000) if options[:time].nil?
 
 client = Mongo::Client.new(['127.0.0.1:27017'], :database => 'pdashboard', :connect => :direct)
 
+
 # 复用以上变量值
 # 原有request_stat文件处理逻辑
 # collection adpreview_backend_request_stat/adpreview_backend_request_stat_hourly
-filepath_stat="#{options[:path]}/#{filepath}/request_stat"
+filepath_stat = "#{options[:path]}/#{filepath}/request_stat"
 file_stat = File.open(filepath_stat, "r")
 device_dict = {
   "1" => "pc",
-  "2" => "mobile",
+  "2" => "mobile"
 }
-doc_stat = file_stat.reduce({}) do |doc, line|
+config[0]["data"] = file_stat.reduce({}) do |doc, line|
   device, manual, machine = line.split
   doc[device_dict[device]] ||= {}
   doc[device_dict[device]]["adpreview_backend_request_manual"] = manual
   doc[device_dict[device]]["adpreview_backend_request_machine"] = machine
   doc
 end.select{|k| !k.nil?}
-doc_stat["recordTimestamp"] = recordTimestamp
-puts "人工请求/机器请求/后端请求总数"
-puts doc_stat
-
-collName_stat = :adpreview_backend_request_stat
-collName_stat = :adpreview_backend_request_stat_hourly if !options[:time].nil?
-coll_stat = client[collName_stat, :capped => true]
-coll_stat.delete_many(:recordTimestamp => doc_stat["recordTimestamp"])
-coll_stat.insert_one(doc_stat)
-result_stat = coll_stat.find()
-puts result_stat
 
 # db = Mongo::Connection.new("localhost", "27017").db("pdashboard")
 # db.collection_names.each {|x| puts x}
@@ -87,8 +116,8 @@ puts result_stat
 # 召回率/准确率
 # 数据文件 recall_result/precision_result
 # collection adpreview_backend_recall_precision/adpreview_backend_recall_precision_hourly
-filepath_recall="#{options[:path]}/#{filepath}/recall_result"
-filepath_precision="#{options[:path]}/#{filepath}/precision_result"
+filepath_recall = "#{options[:path]}/#{filepath}/recall_result"
+filepath_precision = "#{options[:path]}/#{filepath}/precision_result"
 file_recall = File.open(filepath_recall, "r")
 file_precision = File.open(filepath_precision, "r")
 
@@ -105,74 +134,53 @@ doc_precision = file_precision.reduce({}) do |doc, line|
   doc
 end.select{|k| !k.nil?}
 
-doc_recall_precision = {
+config[1]["data"] = {
   "recall" => doc_recall, # 召回率
   "precision" => doc_precision, # 准确率
 }
 
-doc_recall_precision["recordTimestamp"] = recordTimestamp
-puts "召回率/准确率"
-puts doc_recall_precision
-
-collName_recall_precision = :adpreview_backend_recall_precision
-collName_recall_precision = :adpreview_backend_recall_precision_hourly if !options[:time].nil?
-coll_recall_precision = client[collName_recall_precision, :capped => true]
-coll_recall_precision.delete_many(:recordTimestamp => doc_recall_precision["recordTimestamp"])
-coll_recall_precision.insert_one(doc_recall_precision)
-result_recall_precision = coll_recall_precision.find()
-puts result_recall_precision
 
 # 单个策略贡献/累计策略贡献/策略名映射
 # 数据文件 single_strategy_data/acc_strategy_data/strategy_map
 # collection adpreview_backend_strategy_contribution/adpreview_backend_strategy_contribution_hourly
-filepath_s_strategy="#{options[:path]}/#{filepath}/single_strategy_data" # 单个
-filepath_a_strategy="#{options[:path]}/#{filepath}/acc_strategy_data" # 累计
-filepath_n_strategy="#{options[:path]}/#{filepath}/strategy_map" # 策略名
+filepath_s_strategy = "#{options[:path]}/#{filepath}/single_strategy_data" # 单个
+filepath_a_strategy = "#{options[:path]}/#{filepath}/acc_strategy_data" # 累计
+filepath_n_strategy = "#{options[:path]}/#{filepath}/strategy_map" # 策略名
 file_s_strategy = File.open(filepath_s_strategy, "r")
 file_a_strategy = File.open(filepath_a_strategy, "r")
 file_n_strategy = File.open(filepath_n_strategy, "r")
 
 doc_s_strategy = file_s_strategy.reduce({}) do |doc, line|
   id, number = line.split
-  if number.nil # number为空-第一行为total总计
+  if number.nil? # number为空-第一行为total总计
     doc["total"] = id
   else
-    doc[:id] = number
+    doc["id"] = number
   end
   doc
 end.select{|k| !k.nil?}
 
 doc_a_strategy = file_a_strategy.reduce({}) do |doc, line|
   id, number = line.split
-  if number.nil
+  if number.nil?
     doc["total"] = id
   else
-    doc[:id] = number
+    doc["id"] = number
   end
   doc
 end.select{|k| !k.nil?}
 
 doc_n_strategy = file_n_strategy.reduce({}) do |doc, line|
   id, name = line.split
-  doc[:id] = name
+  doc["id"] = name
   doc
 end.select{|k| !k.nil?}
 
-doc_strategy = {
-  "single" => doc_recall, # 单个策略贡献
-  "acc" => doc_precision, # 累计策略贡献
-  "name" => doc_precision, # 策略名map
+config[2]["data"] = {
+  "single" => doc_s_strategy, # 单个策略贡献
+  "acc" => doc_a_strategy, # 累计策略贡献
+  "name" => doc_n_strategy, # 策略名map
 }
 
-doc_strategy["recordTimestamp"] = recordTimestamp
-puts "单个策略贡献/累计策略贡献/策略名映射"
-puts doc_strategy
-
-collName_strategy = :adpreview_backend_strategy_contribution
-collName_strategy = :adpreview_backend_strategy_contribution_hourly if !options[:time].nil?
-coll_strategy = client[collName_strategy, :capped => true]
-coll_strategy.delete_many(:recordTimestamp => doc_strategy["recordTimestamp"])
-coll_strategy.insert_one(doc_strategy)
-result_strategy = coll_strategy.find()
-puts result_strategy
-
+# 调用Function 统一入库
+db_insert_data(options, config, recordTimestamp, client)
